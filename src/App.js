fb1e22b33fb2214185be397f5a9c4b1728b490f5
subhankarp <subhankarp@juniper.net>
2021-02-07 14:23:09 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -4,18 +4,42 @@ import Header from './Header';
 
 class App extends Component {
   render() {
+    // Subscribers data list
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shilpa Bhat",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Srishti Gupta",
+        phone: "9999999999"
+      },
+      {
+        id: 3,
+        name: "Subhankar Pradhan",
+        phone: "10000000000"
+      }
+    ];
+
     return (
-      <div>
+      <div className="container">
         <Header/>
-        <button>Add</button>
-        <div>
+        <button className="addBtn">Add</button>
+        <div className="infoWrapper">
           <span>Name</span> 
-          <br />
           <span>Phone</span>
         </div>
+        {(subscribers && subscribers.length > 0) && subscribers.map(sub => {
+          return (
+            <div className="infoWrapper" key={sub.id}>
+              <div>{sub.name}</div>
+              <div>{sub.phone}</div>
+            </div>
+          )})}
       </div>
     );
-  }
-}
-
+}};
+    
 export default App;
